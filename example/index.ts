060aa4f588c7acbee2706dabb2145051114c9d9b
FahadAshraf26 <fahad.ashraf244636@gmail.com>
2023-09-19 17:11:39 +0500

    fix: example to upload images

--- example/index.ts
+++ example/index.ts
@@ -1,7 +1,13 @@
 import { MulterStorageEngine, StorageType } from './../src/index';
+import express from 'express';
+
+const app = express();
 
 const upload = MulterStorageEngine({
   storage: StorageType.GCP,
 });
 
-upload.single('example');
+app.post('/upload', upload.single('profileImage'), (req, res) => {
+  console.log(req.files);
+  res.json(req.files);
+});
