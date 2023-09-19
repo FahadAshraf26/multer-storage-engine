1edbf6b99ef1441400336bf8ee923f24c4764e38
FahadAshraf26 <fahad.ashraf244636@gmail.com>
2023-09-19 20:25:05 +0500

    fix: test cases

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import { Options, CustomFileResult, FileType, StorageType } from './index.d';
+import { Options, CustomFileResult, FileType } from './index.d';
 import { GCPCustomStorageEngine } from './GCP/GCPCustomStorage';
 import { Request } from 'express';
 import multer from 'multer';
 
+enum StorageType {
+  GCP = 'GCP',
+}
 export const storageEngine = (opts: Options) => {
+  console.log(typeof StorageType);
+
   if (opts.storage === StorageType.GCP) {
     return new GCPCustomStorageEngine();
   }
